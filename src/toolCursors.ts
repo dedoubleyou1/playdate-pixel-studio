adb83730a53f3bad310ec4779b13ae9d2860063e
David Wallin <dedoubleyou1@gmail.com>
2026-05-07 11:43:16 -0400

    Use composed dither cursor icon

--- src/toolCursors.ts
+++ src/toolCursors.ts
@@ -1,10 +1,10 @@
 import eraserCursor from "lucide-static/icons/eraser.svg?url";
-import gridCursor from "lucide-static/icons/grid-2x2.svg?url";
 import paintBucketCursor from "lucide-static/icons/paint-bucket.svg?url";
 import pencilCursor from "lucide-static/icons/pencil.svg?url";
 import slashCursor from "lucide-static/icons/slash.svg?url";
 import squareCursor from "lucide-static/icons/square.svg?url";
 import type { Tool } from "./domain/types";
+import ditherCursor from "./assets/cursors/dither-pencil-ellipsis.svg?url";
 
 const TOOL_CURSORS: Record<Tool, string> = {
   pencil: `url("${pencilCursor}") 4 20, crosshair`,
@@ -12,7 +12,7 @@ const TOOL_CURSORS: Record<Tool, string> = {
   fill: `url("${paintBucketCursor}") 19 20, crosshair`,
   line: `url("${slashCursor}") 5 19, crosshair`,
   rect: `url("${squareCursor}") 5 6, crosshair`,
-  dither: `url("${gridCursor}") 4 4, crosshair`,
+  dither: `url("${ditherCursor}") 4 20, crosshair`,
 };
 
 export function toolCursor(tool: Tool): string {
